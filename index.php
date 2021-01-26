ace4ea285171b66694bf4977fb7f57387016c670
Viktory <blacklastday@gmail.com@git config --global user.email blacklastday@gmail.comgit config --global user.email blacklastday@gmail.com@>
2021-01-26 20:27:59 +0300

    Первый коммит

---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE htm1>
+<html>
+	<head>
+		<title> Моя страничка </title>
+		<link rel="stylesheet" href="mycss.css">
+		<link rel="shortcut icon" href="favicon.ico">
+	</head>
+	<body>
+		<div class="mydiv1">
+			<?php
+				echo "<h1>Разность квадратов чисел 8 и 4 равно  ", (8-4)*(8+4);"</h1>"
+			?>
+		</div>
+	</body>
+</html>
